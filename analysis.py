d87c3fd9f31c3ae160fa15338266d2bf74ab1771
udawe78 <udawe78@gmail.com>
2022-07-23 01:13:16 +0300

    stage 2/5

--- analysis.py
+++ analysis.py
@@ -3,11 +3,22 @@ from os import listdir
 
 pd.set_option('display.max_columns', 8)
 
+# Read CSV files with datasets from the specified folder
 path = 'test/'
 files = [path + file for file in listdir(path)]
-
 data = [pd.read_csv(file) for file in files]
 
-for item in data:
-    print(item.head(20))
+# Change the column names. All column names in the sports and prenatal tables
+# must match the column names in the general table
+for item in data[1:]:
+    item.columns = data[0].columns
 
+# Merge the data frames into one
+df = pd.concat(data, ignore_index=True)
+
+# Delete the Unnamed: 0 column
+df.drop(columns=df.columns[0], inplace=True)
+
+# Print random 20 rows of the resulting data frame.
+# For the reproducible output - 'random_state=30'
+print(df.sample(n=20, random_state=30))
