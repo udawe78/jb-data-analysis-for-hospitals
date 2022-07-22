b81f50e6d80f2e5da5970210582ad4eefa331976
udawe78 <udawe78@gmail.com>
2022-07-22 13:59:15 +0300

    stage 1/5

--- analysis.py
+++ analysis.py
@@ -1,1 +1,12 @@
+import pandas as pd
+from os import listdir
 
+pd.set_option('display.max_columns', 8)
+
+path = 'test/'
+files = [path + file for file in listdir(path)]
+
+data = [pd.read_csv(file) for file in files]
+
+for item in data:
+    print(item.head(20))
