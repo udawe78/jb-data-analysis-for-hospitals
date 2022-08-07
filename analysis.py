56f7843aaeb997bd75aa4bc7c78243b674d652cd
udawe78 <udawe78@gmail.com>
2022-08-08 00:10:38 +0300

    stage_4_of_5

--- analysis.py
+++ analysis.py
@@ -1,7 +1,16 @@
 import pandas as pd
 from os import listdir
 
-pd.set_option('display.max_columns', 9)  # 1.
+
+def what_share(dataset, hospital, diagnosis):
+    filter_hospital = (dataset['hospital'] == hospital)
+    filter_diagnosis = (dataset['diagnosis'] == diagnosis)
+    share = dataset[filter_hospital & filter_diagnosis].shape[0] / dataset[filter_hospital].shape[0]
+    return share
+
+
+# 1 set option
+pd.set_option('display.max_columns', 9)
 
 # 2. Read CSV files with datasets from the specified folder
 path = 'test/'
@@ -36,9 +45,34 @@ cols = ['bmi', 'diagnosis', 'blood_test',
 magic_number = 0
 df[cols] = df[cols].fillna(magic_number)
 
-# 10. Print shape of the resulting data frame
-print(f'Data shape: {df.shape}')
+# Below are the answers to 5 questions
+# list of the answers
+num_answer = ('1st', '2nd', '3rd', '4th', '5th')
+answers = list()
 
-# 11. Print random 20 rows of the resulting data frame
-# for the reproducible output - 'random_state=30'
-print(df.sample(n=20, random_state=30))
+# 1st question: which hospital has the highest number of patients?
+answers.append(df['hospital'].value_counts().idxmax())
+
+# 2nd and 3rd questions: what share of the patients in the general/sports hospital
+#                        suffers from stomach/dislocation-related issues?
+#                        Round the result to the third decimal place
+conditions = (('general', 'stomach'), ('sports', 'dislocation'))
+for item in conditions:
+    answers.append(round(what_share(df, item[0], item[1]), 3))
+
+# 4th question: hat is the difference in the median ages of the patients
+#               in the general and sports hospitals?
+temp_df = df.groupby('hospital').aggregate({'age': 'median'})
+answers.append(temp_df.loc['general', 'age'] - temp_df.loc['sports', 'age'])
+
+# 5th question: In which hospital the blood test was taken
+#               the most often (there is the biggest number of t
+#               in the blood_test column among all the hospitals)?
+#               How many blood tests were taken?
+filter_blood_test = (df['blood_test'] == 't')
+temp_df = df[filter_blood_test].groupby('hospital').aggregate({'blood_test': 'count'})
+answers.append(f"{temp_df['blood_test'].idxmax()}, {temp_df['blood_test'].max()} blood test")
+
+# printing answers
+for k, item in zip(num_answer, answers):
+    print(f'The answer to the {k} question is {item}')
