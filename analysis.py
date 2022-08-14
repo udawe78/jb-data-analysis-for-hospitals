7fab624fb68d3f51419f53673fa71f8445c58c4c
udawe78 <udawe78@gmail.com>
2022-08-14 17:09:15 +0300

    stage_4_of_5

--- analysis.py
+++ analysis.py
@@ -19,15 +19,15 @@ pd.set_option('display.max_columns', 9)
 # 2. Read CSV files with datasets from the specified folder
 path = 'test/'
 files = [path + file for file in listdir(path)]
-data = [pd.read_csv(file) for file in files]
+tables = [pd.read_csv(file) for file in files]
 
 # 3. Change the column names. All column names in the sports and prenatal tables
 # must match the column names in the general table
-for item in data[1:]:
-    item.columns = data[0].columns
+for table in tables[1:]:
+    table.columns = tables[0].columns
 
 # 4. Merge the data frames into one
-df = pd.concat(data, ignore_index=True)
+df = pd.concat(tables, ignore_index=True)
 
 # 5. Delete the Unnamed: 0 column
 df.drop(columns=df.columns[0], inplace=True)
