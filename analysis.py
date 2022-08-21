604111393a08b734c369395c2e253570a7fda9a5
udawe78 <udawe78@gmail.com>
2022-08-21 22:36:46 +0300

    stage 5/5

--- analysis.py
+++ analysis.py
@@ -1,12 +1,14 @@
 import pandas as pd
 from os import listdir
+from matplotlib import pyplot as plt
+import matplotlib.ticker as mticker
 
 
 # calculate the share of the patients in the particular hospital
 # suffers from certain issues
 def what_share(dataset, terms: tuple) -> float:
-    hospital, diagnosis = terms
-    filt = (dataset['hospital'] == hospital) & (dataset['diagnosis'] == diagnosis)
+    hospital, diagnos = terms
+    filt = (dataset['hospital'] == hospital) & (dataset['diagnosis'] == diagnos)
     share = dataset[filt].shape[0] / dataset.shape[0]
     return share
 
@@ -28,7 +30,7 @@ for table in tables[1:]:
 
 # 4. Merge the data frames into one
 df = pd.concat(tables, ignore_index=True)
-print(type(df))
+
 # 5. Delete the Unnamed: 0 column
 df.drop(columns=df.columns[0], inplace=True)
 
@@ -71,7 +73,8 @@ for condition in conditions:
 # What is the difference in the median ages of the patients
 # in the general and sports hospitals?
 age_by_hosp = df.groupby('hospital').aggregate({'age': 'median'})
-answers.append(age_by_hosp.loc['general', 'age'] - age_by_hosp.loc['sports', 'age'])
+difference = age_by_hosp.loc['general'] - age_by_hosp.loc['sports']
+answers.append(difference['age'])
 
 # 5th QUESTION
 # What is the biggest number of t in the blood_test column among all the hospitals?
@@ -80,6 +83,37 @@ filter_blood_test = (df['blood_test'] == 't')
 btest_by_hosp = df[filter_blood_test].groupby('hospital').aggregate({'blood_test': 'count'})
 answers.append(f"{btest_by_hosp['blood_test'].idxmax()}, {btest_by_hosp['blood_test'].max()} blood test")
 
-# all 5 answers
+# all answers of the stage 4
 for number, answer in zip(numbers, answers):
     print(f'The answer to the {number} question is {answer}')
+
+# below are the answers to stage 5 questions
+answers_2 = list()
+
+# first plot is histogram
+fig1, ax1 = plt.subplots()
+bins = (0, 15, 35, 55, 70, 80, 100)
+ax1.hist(df['age'], bins=bins)
+answers_2.append('15-35')
+
+# second plot is pie chart
+fig2, ax2 = plt.subplots()
+diagnosis = df['diagnosis'].value_counts().index
+ax2.pie(df['diagnosis'].value_counts(), labels=diagnosis)
+answers_2.append(diagnosis[0])
+
+# third plot is violin-type
+fig3, ax3 = plt.subplots()
+hospitals = df['hospital'].unique()
+samples = [df[df['hospital'] == hospital].height for hospital in hospitals]
+ax3.violinplot(samples)
+# Optional line
+ax3.xaxis.set_major_locator(mticker.FixedLocator([1, 2, 3]))
+ax3.set_xticklabels(hospitals)
+answers_2.append('It`s because...')
+
+plt.show()
+
+# all answers of the stage 5
+for number, answer in zip(numbers[:3], answers_2):
+    print(f'The answer to the {number} question: {answer}')
